d12ffb69ac2b0dc800e834214fe3885b13bf5c00
DanielPini <daniel@danielpini.com>
2025-10-20 16:43:50 +0200

    Add server proxy config for /api and for /images

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -8,11 +8,11 @@ const HomePage = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((res) => {
+    axios.get("/api/products").then((res) => {
       setProducts(res.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((res) => {
+    axios.get("/api/cart-items").then((res) => {
       setCart(res.data);
     });
   }, []);
